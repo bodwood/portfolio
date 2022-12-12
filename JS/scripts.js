40029bc8ae5604df18b53368ae91308a3256aaeb
bodwood <bodiewood@hotmail.com>
2022-12-11 20:51:44 -0800

    add cdn for scrolltrigger from GSAP

--- JS/scripts.js
+++ JS/scripts.js
@@ -13,11 +13,24 @@ function intro() {
 
 function introAProgrammer() {
   const t2 = gsap.timeline({
-    defaults: { duration: 3}
+    defaults: { duration: 2}
   })
   .to("#textName1", { opacity: 0})
   .set("#textName1", {innerText: "a programmer."})
   .to("#textName1", {opacity: 1});
 }
+
+function aboutMe(){
+  const t3 = gsap.timeline({
+    defaults: { duration: 2}
+  });
+  t3.to("#aboutMe", {
+    x: '0%',
+    onComplete: () => {
+      document.querySelector('#aboutMeDiv').style.overflow = 'visible';
+    }
+  })
+}
 intro();
 introAProgrammer();
+aboutMe();
